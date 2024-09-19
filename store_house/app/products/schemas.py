61263491565e9b703e3fdefc283f462bfa7de3ae
Yauheni Mukhin <evgen2email@gmail.com>
2024-09-19 21:18:56 +0300

    Product schemas refactor

--- store_house/app/products/schemas.py
+++ store_house/app/products/schemas.py
@@ -1,21 +1,23 @@
 from decimal import Decimal
-from typing import Annotated
+from typing import Annotated, TypeAlias
 from uuid import UUID
 
-from annotated_types import Gt
+from annotated_types import Ge
 from pydantic import BaseModel, PositiveInt
+
+Price: TypeAlias = Annotated[Decimal, Ge(0)]
 
 
 class SProduct(BaseModel):
     id: UUID
     title: str
     description: str
-    price: Annotated[Decimal, Gt(0)]
+    price: Price
     available: PositiveInt
 
 
 class SProductWithoutId(BaseModel):
     title: str
     description: str
-    price: Annotated[Decimal, Gt(0)]
+    price: Price
     available: PositiveInt
