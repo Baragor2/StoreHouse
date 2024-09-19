3abf963f928f0813097582f32cfca1503fe560b6
Yauheni Mukhin <evgen2email@gmail.com>
2024-09-19 20:41:00 +0300

    Post product endpoint

--- store_house/app/products/router.py
+++ store_house/app/products/router.py
@@ -1,9 +1,9 @@
-from uuid import UUID
+from uuid import UUID, uuid4
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from app.products.dao import ProductsDAO
-from app.products.schemas import SProduct
+from app.products.schemas import SProduct, SProductWithoutId
 
 router = APIRouter(
     prefix="/products",
@@ -21,3 +21,14 @@ async def get_products() -> list[SProduct]:
 async def get_product(product_id: UUID) -> SProduct:
     product = await ProductsDAO.find_one_or_none(id=product_id)
     return product
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_product(
+        product: SProductWithoutId,
+) -> dict[str, str]:
+    await ProductsDAO.add(
+        id=uuid4(),
+        **dict(product)
+    )
+    return {"message": "Product created"}
