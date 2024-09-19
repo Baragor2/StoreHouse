bfb4f287aa083d4bf28d529b8c813a10a4168d70
Yauheni Mukhin <evgen2email@gmail.com>
2024-09-19 21:28:55 +0300

    Get product with check not exist function created

--- store_house/app/products/dao.py
+++ store_house/app/products/dao.py
@@ -4,16 +4,24 @@ from sqlalchemy import update
 
 from app.dao.base import BaseDAO
 from app.database import async_session_maker
+from app.exceptions import NoSuchProductException
 from app.products.models import Products
-from app.products.schemas import SProductWithoutId
+from app.products.schemas import SProductWithoutId, SProduct
 
 
 class ProductsDAO(BaseDAO):
     model = Products
 
     @classmethod
+    async def get_product(cls, product_id: UUID) -> SProduct:
+        product = await ProductsDAO.find_one_or_none(id=product_id)
+        if not product:
+            raise NoSuchProductException
+        return product
+
+    @classmethod
     async def update_product(cls, product_id: UUID, product: SProductWithoutId) -> None:
-        await cls.find_one_or_none(id=product_id)
+        await cls.get_product(product_id)
 
         async with async_session_maker() as session:
             update_product_stmt = (
@@ -24,3 +32,16 @@ class ProductsDAO(BaseDAO):
 
         await session.execute(update_product_stmt)
         await session.commit()
+
+    @classmethod
+    async def delete_product(cls, product_id: UUID) -> None:
+        await cls.get_product(product_id)
+
+        async with async_session_maker() as session:
+            delete_product_stmt = (
+                delete(Products)
+                .where(Products.id == product_id)
+            )
+
+            await session.execute(delete_product_stmt)
+            await session.commit()
