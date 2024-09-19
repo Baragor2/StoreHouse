0d8306c9e59994cb97e6d02a78298da1ae718c43
Yauheni Mukhin <evgen2email@gmail.com>
2024-09-19 21:18:30 +0300

    Update product func

--- store_house/app/products/dao.py
+++ store_house/app/products/dao.py
@@ -1,6 +1,26 @@
+from uuid import UUID
+
+from sqlalchemy import update
+
 from app.dao.base import BaseDAO
+from app.database import async_session_maker
 from app.products.models import Products
+from app.products.schemas import SProductWithoutId
 
 
 class ProductsDAO(BaseDAO):
     model = Products
+
+    @classmethod
+    async def update_product(cls, product_id: UUID, product: SProductWithoutId) -> None:
+        await cls.find_one_or_none(id=product_id)
+
+        async with async_session_maker() as session:
+            update_product_stmt = (
+                update(Products)
+                .where(Products.id == product_id)
+                .values(**dict(product))
+            )
+
+        await session.execute(update_product_stmt)
+        await session.commit()
