f1cdd7b5b435ee321ad87e542599fdf4a50caf44
Yauheni Mukhin <evgen2email@gmail.com>
2024-09-20 13:55:50 +0300

    Create order items from list refactor

--- store_house/app/order_items/dao.py
+++ store_house/app/order_items/dao.py
@@ -10,13 +10,17 @@ class OrderItemsDAO(BaseDAO):
     @classmethod
     async def create_order_items_from_list(cls, order_items: list[SOrderItem]) -> None:
         for order_item in order_items:
-            await ProductsDAO.check_enough_products(
-                order_item.product_id,
-                order_item.quantity_in_order,
-            )
-            await ProductsDAO.change_available_quantity(
-                order_item.product_id,
-                order_item.quantity_in_order,
-            )
+            await cls.create_order_item(order_item)
 
-            await cls.add(**dict(order_item))
+    @classmethod
+    async def create_order_item(cls, order_item: SOrderItem) -> None:
+        await ProductsDAO.check_enough_products(
+            order_item.product_id,
+            order_item.quantity_in_order,
+        )
+        await ProductsDAO.change_available_quantity(
+            order_item.product_id,
+            order_item.quantity_in_order,
+        )
+
+        await cls.add(**dict(order_item))
