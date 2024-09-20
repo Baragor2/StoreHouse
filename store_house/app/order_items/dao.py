fd5d13e90750d2a90d74ca622bdddfe6dab5957e
Yauheni Mukhin <evgen2email@gmail.com>
2024-09-20 10:13:56 +0300

    Create order items from list

---
+++ store_house/app/order_items/dao.py
@@ -0,0 +1,22 @@
+from app.dao.base import BaseDAO
+from app.order_items.models import OrderItems
+from app.order_items.schemas import SOrderItem
+from app.products.dao import ProductsDAO
+
+
+class OrderItemsDAO(BaseDAO):
+    model = OrderItems
+
+    @classmethod
+    async def create_order_items_from_list(cls, order_items: list[SOrderItem]) -> None:
+        for order_item in order_items:
+            await ProductsDAO.check_enough_products(
+                order_item.product_id,
+                order_item.quantity_in_order,
+            )
+            await ProductsDAO.change_available_quantity(
+                order_item.product_id,
+                order_item.quantity_in_order,
+            )
+
+            await cls.add(**dict(order_item))
