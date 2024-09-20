678eb8e91b825d906a7c12aeb8f9309b37d5e9c9
Yauheni Mukhin <evgen2email@gmail.com>
2024-09-20 14:44:17 +0300

    Include order router

--- store_house/app/main.py
+++ store_house/app/main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 
 from app.config import settings
 from app.products.router import router as products_router
+from app.orders.router import router as orders_router
 
 app = FastAPI()
 
 app.include_router(products_router)
+app.include_router(orders_router)
 
 if __name__ == "__main__":
     uvicorn.run(
