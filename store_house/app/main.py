deec02542ac5713c2ab8d6ce6685f05609b3e846
Yauheni Mukhin <evgen2email@gmail.com>
2024-09-22 12:58:25 +0300

    Corse middleware added

--- store_house/app/main.py
+++ store_house/app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import settings
 from app.products.router import router as products_router
@@ -17,3 +18,22 @@ if __name__ == "__main__":
         port=settings.run.port,
         reload=True,
     )
+
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
+    allow_headers=[
+        "Content-Type",
+        "Authorization",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+    ],
+)
